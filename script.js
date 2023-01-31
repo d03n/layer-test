e1fb5880b4d80b2982452280932b69bc78c62eeb
d03_np <nattar.1506@gmail.com>
2023-02-01 00:40:52 +0700

    12-18

--- script.js
+++ script.js
@@ -1,68 +1,97 @@
-feather.replace();
+(function () {
+    if (
+    !"mediaDevices" in navigator ||
+    !"getUserMedia" in navigator.mediaDevices
+    ) {
+    alert("Camera API is not available in your browser");
+    return;
+    }
 
-const controls = document.querySelector('.controls');
-const cameraOptions = document.querySelector('.video-options>select');
-const video = document.querySelector('video');
-const canvas = document.querySelector('canvas');
-const screenshotImage = document.querySelector('img');
-const buttons = [...controls.querySelectorAll('button')];
-let streamStarted = false;
+    // get page elements
+    const video = document.querySelector("#video");
+    const btnPlay = document.querySelector("#btnPlay");
+    const btnPause = document.querySelector("#btnPause");
+    const btnScreenshot = document.querySelector("#btnScreenshot");
+    const btnChangeCamera = document.querySelector("#btnChangeCamera");
+    const screenshotsContainer = document.querySelector("#screenshots");
+    const canvas = document.querySelector("#canvas");
+    const devicesSelect = document.querySelector("#devicesSelect");
 
-const [play, pause, screenshot] = buttons;
-
-const constraints = {
+    // video constraints
+    const constraints = {
     video: {
         width: {
-        min: 1080,
+        min: 1280,
         ideal: 1920,
         max: 2560,
         },
         height: {
-        min: 2340,
+        min: 720,
         ideal: 1080,
-        max: 1440
+        max: 1440,
         },
-    }
-};
+    },
+    };
 
-const getCameraSelection = async () => {
-    const devices = await navigator.mediaDevices.enumerateDevices();
-    const videoDevices = devices.filter(device => device.kind === 'videoinput');
-    const options = videoDevices.map(videoDevice => {
-        return `<option value="${videoDevice.deviceId}">${videoDevice.label}</option>`;
+    // use front face camera
+    let useFrontCamera = true;
+
+    // current video stream
+    let videoStream;
+
+    // handle events
+    // play
+    btnPlay.addEventListener("click", function () {
+    video.play();
+    btnPlay.classList.add("is-hidden");
+    btnPause.classList.remove("is-hidden");
     });
-    cameraOptions.innerHTML = options.join('');
-};
 
-play.onclick = () => {
-    if (streamStarted) {
-        video.play();
-        play.classList.add('d-none');
-        pause.classList.remove('d-none');
-        return;
+    // pause
+    btnPause.addEventListener("click", function () {
+    video.pause();
+    btnPause.classList.add("is-hidden");
+    btnPlay.classList.remove("is-hidden");
+    });
+
+    // take screenshot
+    btnScreenshot.addEventListener("click", function () {
+    const img = document.createElement("img");
+    canvas.width = video.videoWidth;
+    canvas.height = video.videoHeight;
+    canvas.getContext("2d").drawImage(video, 0, 0);
+    img.src = canvas.toDataURL("image/png");
+    screenshotsContainer.prepend(img);
+    });
+
+    // switch camera
+    btnChangeCamera.addEventListener("click", function () {
+    useFrontCamera = !useFrontCamera;
+
+    initializeCamera();
+    });
+
+    // stop video stream
+    function stopVideoStream() {
+    if (videoStream) {
+        videoStream.getTracks().forEach((track) => {
+        track.stop();
+        });
     }
-    if ('mediaDevices' in navigator && navigator.mediaDevices.getUserMedia) {
-        const updatedConstraints = {
-            ...constraints,
-            deviceId: {
-            exact: cameraOptions.value
-            }
-        };
-        startStream(updatedConstraints);
     }
-};
 
-const startStream = async (constraints) => {
-    const stream = await navigator.mediaDevices.getUserMedia(constraints);
-    handleStream(stream);
-};
+    // initialize
+    async function initializeCamera() {
+    stopVideoStream();
+    constraints.video.facingMode = useFrontCamera ? "user" : "environment";
 
-const handleStream = (stream) => {
-    video.srcObject = stream;
-    play.classList.add('d-none');
-    pause.classList.remove('d-none');
-    screenshot.classList.remove('d-none');
-    streamStarted = true;
-};
+    try {
+        videoStream = await navigator.mediaDevices.getUserMedia(constraints);
+        video.srcObject = videoStream;
+    } catch (err) {
+        alert("Could not access the camera");
+    }
+    }
 
-getCameraSelection();
+    initializeCamera();
+})();
